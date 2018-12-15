f31de892431b800beb1c4daf98e026ff31b8e0df
wencun <810815091@qq.com>
2018-12-15 15:00:14 +0800

    MySpringMVC

---
+++ src/main/java/com/wen/core/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.wen.core.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wen.annotation.MyController;
+import com.wen.annotation.MyRequestMapping;
+import com.wen.annotation.MyRequestParam;
+
+@MyController
+@MyRequestMapping("/test")
+public class TestController {
+
+	@MyRequestMapping("/doTest")
+	public void test1(HttpServletRequest request, HttpServletResponse response, @MyRequestParam("param") Integer param) {
+		System.out.println(param);
+		try {
+			response.getWriter().write("doTest method success! param:" + param);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@MyRequestMapping("/doTest2")
+	public void test2(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			response.getWriter().println("doTest2 method success!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
